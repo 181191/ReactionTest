01c0630ef973f9aed3186b8168ccda0867b7ae26
EirikT <181191@stud.hvl.no>
2019-03-25 14:29:37 +0100

    funk

--- ReactionTest/ReactionTest/MainPage.xaml.cs
+++ ReactionTest/ReactionTest/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,11 +10,33 @@ namespace ReactionTest
 {
     public partial class MainPage : ContentPage
     {
+        private bool buttonActive; 
+
         public MainPage()
         {
             InitializeComponent();
 
-            string buttonString = "hei"; 
+            
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            if(isActive())
+            {
+                infoString.Text = "Miss";
+            }
+            else
+            {
+                infoString.Text = "Hit"; 
+                
+            }
+
+
+        }
+
+        private bool isActive()
+        {
+            return false; 
         }
     }
 }
