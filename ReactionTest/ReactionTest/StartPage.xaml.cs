c2144817bff766e9a55df59014d6efd4777e0328
EirikT <181191@stud.hvl.no>
2019-04-03 11:43:19 +0200

    lol

---
+++ ReactionTest/ReactionTest/StartPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReactionTest
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StartPage : ContentPage
+	{
+		public StartPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ThreeMinuteTestClicked(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new MainPage(3));
+
+        }
+
+        private void SevenMinuteTestClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage(7));
+        }
+
+        private void TenMinuteTestClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage(10));
+        }
+    }
+}
